b125a5afbd5e3a07cc78f02d5868d40774e5889d
Glitch (image-gallery) <none>
2017-12-17 22:33:23 +0000

    🚗🚏 Updated with Glitch

---
+++ images.js
@@ -0,0 +1,26 @@
+module.exports = [
+  // {
+  //   url: 'https://cdn.glitch.com/251d5f74-198e-460c-bf75-e22e768c4d03%2F01.jpg',
+  //   imageWidth: 736,
+  //   imageHeight: 1084,
+  //   imageTitle: 'This is image #1'
+  // },
+  // {
+  //   url: 'https://cdn.glitch.com/251d5f74-198e-460c-bf75-e22e768c4d03%2F02.jpg',
+  //   imageWidth: 499,
+  //   imageHeight: 613,
+  //   imageTitle: 'This is image #2'
+  // },
+  // {
+  //   url: 'https://cdn.glitch.com/251d5f74-198e-460c-bf75-e22e768c4d03%2F04.jpeg',
+  //   imageWidth: 2000,
+  //   imageHeight: 1126,
+  //   imageTitle: 'This is image #3'
+  // },
+  // {
+  //   url: 'https://cdn.glitch.com/251d5f74-198e-460c-bf75-e22e768c4d03%2F03.jpeg',
+  //   imageWidth: 2000,
+  //   imageHeight: 1333,
+  //   imageTitle: 'This is image #4'
+  // }
+];
